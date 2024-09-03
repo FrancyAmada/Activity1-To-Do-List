ea2d4921049e788bec33e42d548e7035fdd3a8e6
FrancyAmada <francyamada1983@gmail.com>
2024-09-03 14:16:11 +0800

    Added Checkbox

--- src/task.ts
+++ src/task.ts
@@ -17,44 +17,48 @@ export class Task {
         this.app_list.removeChild(this.list_item)
     }
 
-    updateTask = () => {
+    updateTask = (check_box: HTMLInputElement) => {
         this.completed = !this.completed
         if (!this.completed) {
+            check_box.checked = false
             const content = this.list_item.getElementsByClassName('task completed').item(0)!
             content.setAttribute("class", "task")
         } else {
+            check_box.checked = true
             const content = this.list_item.getElementsByClassName('task').item(0)!
             content.setAttribute("class", "task completed")
         }
     }
 
-    setupButtons = (check_button: HTMLButtonElement, delete_button: HTMLButtonElement) => {
-        check_button.addEventListener('click', () => this.updateTask())
+    setupButtons = (task_content: HTMLDivElement, delete_button: HTMLButtonElement, check_box: HTMLInputElement) => {
+        const check_box_element = check_box
+
+        task_content.addEventListener('click', () => this.updateTask(check_box_element))
         delete_button.addEventListener('click', () => this.removeTask())
     }
 
     createListItem = (info: string) => {
         const li = document.createElement("li")
         const content = document.createElement("div")
+        const check_box = document.createElement("input")
         const info_text_element = document.createElement("p")
-        const check_button = document.createElement("button")
         const delete_button = document.createElement("button")
 
         li.setAttribute("id", info.replace(/\s+/g, ''))
         content.setAttribute("class", "task")
+        check_box.setAttribute("type", "checkbox")
         info_text_element.setAttribute("class", "taskInfo")
-        check_button.setAttribute("class", "taskCompletionButton")
         delete_button.setAttribute("class", "delete")
         
         delete_button.textContent = "Delete"
         info_text_element.textContent = info
 
-        check_button.appendChild(info_text_element)
-        content.appendChild(check_button)
+        content.appendChild(check_box)
+        content.appendChild(info_text_element)
         content.appendChild(delete_button)
         li.appendChild(content)
 
-        this.setupButtons(check_button, delete_button)
+        this.setupButtons(content, delete_button, check_box)
         return li
     }
 }
